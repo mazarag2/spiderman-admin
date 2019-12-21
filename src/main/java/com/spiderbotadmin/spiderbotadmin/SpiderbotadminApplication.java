bf08c0fc1e5f44bbee288333b89aea9852f2079d
Michael Zaragoza <mazarag2@gmail.com>
2019-12-20 17:15:41 -0700

    frist commit

---
+++ src/main/java/com/spiderbotadmin/spiderbotadmin/SpiderbotadminApplication.java
@@ -0,0 +1,13 @@
+package com.spiderbotadmin.spiderbotadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderbotadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderbotadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spiderbotadmin/spiderbotadmin/SpiderbotadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spiderbotadmin.spiderbotadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiderbotadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
